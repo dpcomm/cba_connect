77921cd6868fe298b9263fb6f23ae87750c98bd4
Gusq1s <phyunbinp@gmail.com>
2026-01-11 01:51:20 +0900

    [Add] login과정에 register expo token usecase 연결

--- src/shared/di/container.ts
+++ src/shared/di/container.ts
@@ -3,14 +3,17 @@ import { LoginUseCase } from '@application/auth/LoginUseCase';
 import { LogoutUseCase } from '@application/auth/LogoutUseCase';
 import { RefreshUseCase } from '@application/auth/RefreshUseCase';
 import { RegisterUseCase } from '@application/auth/RegisterUseCase';
+import { RegisterExpoTokenUseCase } from '@application/notification/RegistExpoTokenUseCase';
 import { GetMeUseCase } from '@application/user/GetMeUseCase';
 import { AuthRepository } from '@infrastructure/auth/AuthRepository';
+import { ApiExpoPushTokenRepository } from '@infrastructure/notification/ApiExpoPushTokenRepository';
 import { UserRepository } from '@infrastructure/user/UserRepository';
 import 'reflect-metadata';
 import { container } from 'tsyringe';
 
 container.register('AuthRepository', { useClass: AuthRepository });
 container.register('UserRepository', { useClass: UserRepository });
+container.register('ExpoPushTokenRepository', { useClass: ApiExpoPushTokenRepository})
 
 container.register(LoginUseCase, { useClass: LoginUseCase });
 container.register(RegisterUseCase, { useClass: RegisterUseCase });
@@ -18,5 +21,7 @@ container.register(LogoutUseCase, { useClass: LogoutUseCase });
 container.register(RefreshUseCase, { useClass: RefreshUseCase });
 container.register(AutoLoginUseCase, { useClass: AutoLoginUseCase });
 container.register(GetMeUseCase, { useClass: GetMeUseCase });
+container.register(RegisterExpoTokenUseCase, { useClass: RegisterExpoTokenUseCase})
 
 export { container };
+
